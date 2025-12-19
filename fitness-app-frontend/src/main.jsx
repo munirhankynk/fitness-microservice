55e39d789ec1dcd3452d90fc0ed065a316b4ad34
munirhankynk <munirhankynk@gmail.com>
2025-12-19 20:44:07 +0300

    Frontend autharization settings

--- fitness-app-frontend/src/main.jsx
+++ fitness-app-frontend/src/main.jsx
@@ -2,14 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import { Provider } from 'react-redux'
-import store from './store/store'
+import { store } from './store/store'
+import { AuthProvider } from 'react-oauth2-code-pkce'
+import { authConfig } from './authConfig'
 
 import App from './App'
 
-// As of React 18
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <AuthProvider authConfig={authConfig}
+                loadingComponent={<div>Loading...</div>}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </AuthProvider>,
 )
